dbfbd08f1189248f580ee3cbbb54dd409d85a617
Topher Brennan <topher.t.brennan@gmail.com>
2025-07-27 06:21:43 -0700

    Constants based on factors of 7.

--- src/constants.rs
+++ src/constants.rs
@@ -1,15 +1,15 @@
 
 pub const KC: f32 = HEX_SIZE / 100.0; // capacity coefficient
-pub const KE: f32 = 0.05;  // erosion rate fraction
-pub const KD: f32 = 0.05;  // deposition rate fraction
-pub const FLOW_FACTOR: f32 = 0.9;
+pub const KE: f32 = 1.0 / 14.0;  // erosion rate fraction
+pub const KD: f32 = 1.0 / 14.0;  // deposition rate fraction
+pub const FLOW_FACTOR: f32 = 6.0 / 7.0;
 pub const MAX_SLOPE: f32 = 1.0; // Prevents runaway erosion by capping slope used in capacity calc
 pub const MAX_FLOW: f32 = WIDTH_HEXAGONS as f32;
 pub const MAX_ELEVATION: f32 = (WIDTH_HEXAGONS as f32) * 4.0 + HEX_SIZE / 100.0;
 pub const SEA_LEVEL: f32 = (WIDTH_HEXAGONS as f32) * 2.0;
 pub const HEX_SIZE: f32 = 2640.0; // Feet
 pub const RIVER_WATER_PER_STEP: f32 = 37.1; // Feet
-pub const RIVER_LOAD_FACTOR: f32 = 1.2;
+pub const RIVER_LOAD_FACTOR: f32 = 0.4;
 pub const RAIN_PER_STEP: f32 = 1.0 / 12.0 / 365.0 / 24.0; // Feet
 pub const DEFAULT_ROUNDS: u32 = 1000;
 pub const WATER_THRESHOLD: f32 = 1.0 / 12.0; // One inch in feet
